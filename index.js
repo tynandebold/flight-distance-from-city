aaa7fe97a8a197dac8ba0d4750da39d3cd60225b
Tynan DeBold <tynan.debold@tradeshift.com>
2019-03-07 20:38:49 +0100

    finishing up

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-(function () {
+(function() {
   var location;
   var rotation;
-  var ranges;
-  var path;
-  var projection = d3.geoOrthographic();
+  var legends;
+  var range, ranges;
 
   var airports = {
     copenhagen: {
@@ -15,7 +14,7 @@
       rotation: [-114, -25, 0]
     },
     newyork: {
-      coords: [-74.0060, 40.7128],
+      coords: [-74.006, 40.7128],
       rotation: [74, -40, 0]
     }
   };
@@ -26,35 +25,107 @@
     location = airports[e.target.value].coords;
     rotation = airports[e.target.value].rotation;
 
-    addDetails();
+    update(flightTimes, location, rotation);
   }
 
-  var svg = d3.select("svg");
+  function render() {
+    var width = Math.min(960, window.innerWidth);
+    var height = Math.min(600, window.innerHeight);
+    var margin = 25;
+    projection.fitExtent(
+      [[margin, margin], [width - margin, height - margin]],
+      {
+        type: 'Sphere'
+      }
+    );
+    svg.attr('width', width).attr('height', height);
+    svg.selectAll('path').attr('d', path);
+  }
 
-  var map = svg.select('#map');
+  var projection = d3.geoOrthographic().clipAngle(90);
 
-  map.append('path')
-    .datum({ type: "Sphere" })
-    .attr('fill', '#eee')
-    .attr('stroke', '#111');
+  var path = d3.geoPath().projection(projection);
 
-  map.append('path')
+  var svg = d3.select('svg');
+
+  var sphere = svg
+    .append('path')
+    .attr('class', 'outline')
+    .attr('style', 'filter:url(#dropshadow)')
+    .datum({ type: 'Sphere' });
+
+  svg
+    .append('path')
     .datum(d3.geoGraticule())
     .attr('fill', 'none')
-    .attr('stroke', '#999');
+    .attr('stroke-width', 0.5)
+    .attr('stroke', '#ddd');
 
-  var countries = map.append('path')
-    .attr('id', 'countries');
+  var countries = svg.append('path').attr('id', 'countries');
 
-  d3.json('countries.geojson', function (err, json) {
-    countries.datum(d3.geoStitch(json));
-    addDetails();
-    render();
-  });
+  d3.queue()
+    .defer(d3.json, 'countries.geojson')
+    .await((err, world) => {
+      countries
+        .datum(d3.geoStitch(world))
+        .attr('opacity', 0.01)
+        .transition()
+        .duration(500)
+        .attr('opacity', 0.6);
+
+      render();
+    });
+
+  function update(flightTimes, location, rotation) {
+    if (legends) legends.classed('hover', false);
+    d3.select('.ranges').remove();
+    d3.select('.location')
+      .transition(250)
+      .style('opacity', 0)
+      .remove();
+
+    flightTimes.forEach(data => {
+      var time = +data.name.split(' ')[0];
+      var range = 545.267756 * time - 189.306421;
+      data.range = range;
+      return (data.l = ((range / (6371 * 0.62137)) * 180) / Math.PI);
+    });
+
+    flightTimes = flightTimes.sort((a, b) => a - b);
+
+    ranges = svg
+      .append('g')
+      .classed('ranges', true)
+      .selectAll('path')
+      .data(
+        flightTimes.map(d => {
+          var c = d3
+            .geoCircle()
+            .center(location)
+            .radius(d.l)();
+          c.properties = d;
+          return c;
+        })
+      );
+
+    range = ranges
+      .enter()
+      .append('path')
+      .attr('id', (d, i) => 'd' + i);
+
+    ranges.exit().remove();
+
+    svg
+      .append('path')
+      .classed('location', true)
+      .datum({ type: 'Point', coordinates: location });
+
+    projection.rotate(rotation);
+    projection.translate([550, 250]);
+  }
 
   location = [12.5683, 55.6761];
-  rotation = [-10, -60, 0];
-
+  rotation = [-12, -55, 0];
   var flightTimes = d3.csvParse(`name
 16 hours
 12 hours
@@ -63,88 +134,34 @@
 2 hours
 1 hour`);
 
-  flightTimes.forEach(d => {
-    var time = +d.name.split(' ')[0];
-    var range = (545.267756 * time) - 189.306421;
-    d.range = range;
-    return d.l = range / (6371 * 0.62137) * 180 / Math.PI;
-  });
+  update(flightTimes, location, rotation);
 
-  flightTimes = flightTimes.sort((a, b) => a - b);
+  var hover;
 
-  var hover = 0;
-
-  var legends = svg.append('g')
+  // legend code
+  legends = svg
+    .append('g')
     .attr('id', 'legend')
     .selectAll('g')
-    .data(flightTimes.map(d => (d.properties = d, d)))
+    .data(flightTimes.map(d => ((d.properties = d), d)))
     .enter()
     .append('g')
     .attr('transform', (d, i) => `translate(${[0, i * 20 + 60]})`)
     .on('mouseover click', d => {
-      console.log(ranges);
       hover = d.properties.name;
-      ranges.classed('hover', d => d.properties.name == hover);
+      range.classed('hover', d => d.properties.name == hover);
       legends.classed('hover', d => d.properties.name == hover);
     });
 
-  legends.append('rect')
-    .attr('width', 250)
+  legends
+    .append('rect')
+    .attr('width', 200)
     .attr('height', 20)
-    .attr('y', -15)
+    .attr('y', -15);
 
-  var f = d3.formatLocale({ thousands: ",", grouping: [3] }).format(" >6,.2r");
-  legends.append('text')
-    .text(d => `${f(d.range)} miles - ${d.name}`)
-
-  flightTimes = flightTimes.sort((a, b) => b.l - a.l)
-
-  function addDetails() {
-    d3.select('.ranges').transition(500).style('opacity', 0).remove();
-    d3.select('.location').transition(500).style('opacity', 0).remove();
-
-    ranges = map.append('g')
-      .classed('ranges', true)
-      .selectAll('path')
-      .data(flightTimes.map(d => {
-        var c = d3.geoCircle()
-          .center(location)
-          .radius(d.l)
-          ();
-        c.properties = d;
-        return c;
-      }))
-      .enter()
-      .append('path')
-      .attr("id", (d, i) => "d" + i)
-
-    map.append('path')
-      .classed('location', true)
-      .datum({ type: "Point", coordinates: location });
-
-    if (projection.rotate) projection.rotate(rotation);
-    projection.translate([550, 250]);
-    path = d3.geoPath(projection);
-  }
-
-  // svg
-  render = function () {
-    var width = Math.min(960, window.innerWidth);
-    var height = Math.min(600, window.innerHeight);
-    var margin = 25;
-
-    if (width < 400) {
-      height += 100;
-      margin = 10;
-    }
-
-    projection.fitExtent([[(width - height) / 2 - 2 * margin, margin * 2], [width - margin, height - margin]], {
-      type: "Sphere"
-    });
-    svg.attr("width", width).attr("height", height);
-    map.selectAll('path').attr('d', path);
-  }
+  var f = d3.formatLocale({ thousands: ',', grouping: [3] }).format(' >6,.2r');
+  legends.append('text').text(d => `${f(d.range)} miles - ${d.name}`);
 
   d3.geoInertiaDrag(svg, render, projection);
-  d3.interval(render, 500); // for window.resize
+  d3.interval(render, 500);
 })();
